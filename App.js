bc35217d2545a8a7f9e0946961375df2d954b8b2
Sharad Agarwal <obviouslysharad@gmail.com>
2022-01-01 13:18:48 +0530

    Added MapView to the NavAlert app

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import { useState, useEffect } from "react";
+// import GeoLocation from "@react-native-community/geolocation";
+// import Geolocation from "react-native-geolocation-service";
+// navigator.geolocation = require("@react-native-community/geolocation");
 
 export default function App() {
+  const [lat, setLat] = useState("");
+  const [long, setLong] = useState("");
+  // function success(e) {
+  //   console.log(e.coords.longitude);
+  // }
+  // navigator.geolocation.getCurrentPosition(success, error);
+  // GeoLocation.setRNConfiguration(config);
+  // useEffect(() => {
+  //   GeoLocation.getCurrentPosition(
+  //     //Will give you the current location
+  //     (position) => {
+  //       //getting the Longitude from the location json
+  //       console.log(position);
+  //     },
+  //     (error) => alert(error.message),
+  //     {
+  //       enableHighAccuracy: true,
+  //       timeout: 20,
+  //       maximumAge: 1000,
+  //     }
+  //   );
+  // });
+  // Geolocation.getCurrentPosition(
+  //   (position) => {
+  //     console.log(position);
+  //   },
+  //   (error) => {
+  //     // See error code charts below.
+  //     console.log(error.code, error.message);
+  //   },
+  //   { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+  // );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MapView style={{ ...StyleSheet.absoluteFill }}>
+      <Marker
+        coordinate={{ latitude: 27.787, longitude: 76.2 }}
+        title="TestTitle"
+        description="TestDescription"
+      ></Marker>
+    </MapView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#242333",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
